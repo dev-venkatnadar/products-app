5cf6ed2ddf20a8ab71ca577b7cb342be99456bcb
venkatnadar <a.s.venkatnadar@gmail.com>
2026-07-01 21:58:14 +0530

    basic app auth configuration

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full',
+  },
+  {
+      path: '',
+      loadChildren: () =>
+          import('./modules/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+      path: '**',
+      redirectTo: 'login',
+  },
+];
